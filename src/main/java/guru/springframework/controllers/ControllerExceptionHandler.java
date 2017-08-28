f4819bd2922ca3ebb571541b02eba85ad2d4d70b
John Thompson <john@springframework.guru>
2017-08-28 19:21:50 -0400

    removing unused parameters

--- src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
+++ src/main/java/guru/springframework/controllers/ControllerExceptionHandler.java
@@ -1,7 +1,12 @@
 package guru.springframework.controllers;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.support.WebExchangeBindException;
 
 
 /**
@@ -11,18 +16,15 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleNumberFormat(Exception exception){
-//
-//        log.error("Handling Number Format Exception");
-//        log.error(exception.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//
-//        modelAndView.setViewName("400error");
-//        modelAndView.addObject("exception", exception);
-//
-//        return modelAndView;
-//    }
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(WebExchangeBindException.class)
+    public String handleNumberFormat(Exception exception, Model model){
+
+        log.error("Handling Binding Exception");
+        log.error(exception.getMessage());
+
+        model.addAttribute("exception", exception);
+
+        return "400error";
+    }
 }
