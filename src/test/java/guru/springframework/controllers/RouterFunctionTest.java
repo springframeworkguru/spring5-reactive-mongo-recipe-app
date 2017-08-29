23e02f2f06d4bc000c16f5f822e3e57b8232fbe2
John Thompson <john@springframework.guru>
2017-08-29 15:56:02 -0400

    adding webtest client test

---
+++ src/test/java/guru/springframework/controllers/RouterFunctionTest.java
@@ -0,0 +1,62 @@
+package guru.springframework.controllers;
+
+import guru.springframework.config.WebConfig;
+import guru.springframework.domain.Recipe;
+import guru.springframework.services.RecipeService;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import reactor.core.publisher.Flux;
+
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by jt on 8/29/17.
+ */
+
+public class RouterFunctionTest {
+
+    WebTestClient webTestClient;
+
+    @Mock
+    RecipeService recipeService;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        WebConfig webConfig = new WebConfig();
+
+        RouterFunction<?> routerFunction = webConfig.routes(recipeService);
+
+        webTestClient = WebTestClient.bindToRouterFunction(routerFunction).build();
+    }
+
+
+    @Test
+    public void testGetRecipes() throws Exception {
+
+        when(recipeService.getRecipes()).thenReturn(Flux.just());
+
+        webTestClient.get().uri("/api/recipes")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk();
+    }
+
+    @Test
+    public void testGetRecipesWithData() throws Exception {
+
+        when(recipeService.getRecipes()).thenReturn(Flux.just(new Recipe(), new Recipe()));
+
+        webTestClient.get().uri("/api/recipes")
+                .accept(MediaType.APPLICATION_JSON)
+                .exchange()
+                .expectStatus().isOk()
+                .expectBodyList(Recipe.class);
+    }
+}
