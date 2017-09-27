1afd5a0f89a092d939b9aadd0dc24749e357b262
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2017-09-27 18:45:43 +0200

    Refactor UnitOfMeasure service

--- src/test/java/guru/springframework/services/UnitOfMeasureServiceImplTest.java
+++ src/test/java/guru/springframework/services/UnitOfMeasureServiceImplTest.java
@@ -1,48 +1,46 @@
 package guru.springframework.services;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
 import guru.springframework.commands.UnitOfMeasureCommand;
 import guru.springframework.converters.UnitOfMeasureToUnitOfMeasureCommand;
 import guru.springframework.domain.UnitOfMeasure;
 import guru.springframework.repositories.reactive.UnitOfMeasureReactiveRepository;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import reactor.core.publisher.Flux;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.*;
-
+@RunWith(MockitoJUnitRunner.class)
 public class UnitOfMeasureServiceImplTest {
 
-    UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand = new UnitOfMeasureToUnitOfMeasureCommand();
-    UnitOfMeasureService service;
+    private UnitOfMeasureToUnitOfMeasureCommand unitOfMeasureToUnitOfMeasureCommand = new UnitOfMeasureToUnitOfMeasureCommand();
 
     @Mock
-    UnitOfMeasureReactiveRepository unitOfMeasureReactiveRepository;
+    private UnitOfMeasureReactiveRepository unitOfMeasureReactiveRepository;
+
+    private UnitOfMeasureService service;
 
     @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-
+    public void setUp() {
         service = new UnitOfMeasureServiceImpl(unitOfMeasureReactiveRepository, unitOfMeasureToUnitOfMeasureCommand);
     }
 
     @Test
     public void listAllUoms() throws Exception {
         //given
-        Set<UnitOfMeasure> unitOfMeasures = new HashSet<>();
         UnitOfMeasure uom1 = new UnitOfMeasure();
         uom1.setId("1");
-        unitOfMeasures.add(uom1);
 
         UnitOfMeasure uom2 = new UnitOfMeasure();
         uom2.setId("2");
-        unitOfMeasures.add(uom2);
 
         when(unitOfMeasureReactiveRepository.findAll()).thenReturn(Flux.just(uom1, uom2));
 
@@ -50,8 +48,7 @@ public class UnitOfMeasureServiceImplTest {
         List<UnitOfMeasureCommand> commands = service.listAllUoms().collectList().block();
 
         //then
-        assertEquals(2, commands.size());
-        verify(unitOfMeasureReactiveRepository, times(1)).findAll();
+        assertThat(commands).hasSize(2);
+        verify(unitOfMeasureReactiveRepository).findAll();
     }
-
 }
