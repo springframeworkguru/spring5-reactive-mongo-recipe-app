eedb35306035122cc2139a64b9a2e98d35c1c476
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2017-09-27 18:20:57 +0200

    Remove redundant JavaDoc

--- src/main/java/guru/springframework/services/IngredientService.java
+++ src/main/java/guru/springframework/services/IngredientService.java
@@ -3,9 +3,6 @@ package guru.springframework.services;
 import guru.springframework.commands.IngredientCommand;
 import reactor.core.publisher.Mono;
 
-/**
- * Created by jt on 6/27/17.
- */
 public interface IngredientService {
 
     Mono<IngredientCommand> findByRecipeIdAndIngredientId(String recipeId, String ingredientId);
