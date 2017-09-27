e3df5e7efb438c8eab99888226d9209a539dbdb4
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2017-09-27 22:04:03 +0200

    Improve Recipe controller

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -1,61 +1,65 @@
 package guru.springframework.controllers;
 
-import guru.springframework.commands.RecipeCommand;
-import guru.springframework.exceptions.NotFoundException;
-import guru.springframework.services.RecipeService;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
+import static lombok.AccessLevel.PRIVATE;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
 
 import javax.validation.Valid;
 
-/**
- * Created by jt on 6/19/17.
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import guru.springframework.commands.RecipeCommand;
+import guru.springframework.exceptions.NotFoundException;
+import guru.springframework.services.RecipeService;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import lombok.extern.slf4j.Slf4j;
+
 @Slf4j
 @Controller
+@FieldDefaults(level = PRIVATE, makeFinal = true)
+@RequiredArgsConstructor
 public class RecipeController {
 
     private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
-    private final RecipeService recipeService;
 
-    public RecipeController(RecipeService recipeService) {
-        this.recipeService = recipeService;
-    }
+    RecipeService recipeService;
 
     @GetMapping("/recipe/{id}/show")
-    public String showById(@PathVariable String id, Model model){
-
+    public String showById(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findById(id).block());
 
         return "recipe/show";
     }
 
     @GetMapping("recipe/new")
-    public String newRecipe(Model model){
+    public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
 
         return "recipe/recipeform";
     }
 
     @GetMapping("recipe/{id}/update")
-    public String updateRecipe(@PathVariable String id, Model model){
+    public String updateRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(id).block());
         return RECIPE_RECIPEFORM_URL;
     }
 
     @PostMapping("recipe")
-    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult){
-
-        if(bindingResult.hasErrors()){
-
-            bindingResult.getAllErrors().forEach(objectError -> {
-                log.debug(objectError.toString());
-            });
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand command, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors()
+                    .stream()
+                    .map(Object::toString)
+                    .forEach(log::debug);
 
             return RECIPE_RECIPEFORM_URL;
         }
@@ -66,18 +70,16 @@ public class RecipeController {
     }
 
     @GetMapping("recipe/{id}/delete")
-    public String deleteById(@PathVariable String id){
-
-        log.debug("Deleting id: " + id);
+    public String deleteById(@PathVariable String id) {
+        log.debug("Deleting id: {}", id);
 
         recipeService.deleteById(id);
         return "redirect:/";
     }
 
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(NOT_FOUND)
     @ExceptionHandler(NotFoundException.class)
-    public ModelAndView handleNotFound(Exception exception){
-
+    public ModelAndView handleNotFound(Exception exception) {
         log.error("Handling not found exception");
         log.error(exception.getMessage());
 
