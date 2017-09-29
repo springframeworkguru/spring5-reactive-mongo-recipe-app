cd02705124c723218b9dd187c399239c341c3dce
Andrii Plotnikov <plotnikovandrey1990@gmail.com>
2017-09-29 20:10:01 +0200

    Improve CategoryCommandToCategory

--- src/test/java/guru/springframework/converters/CategoryCommandToCategoryTest.java
+++ src/test/java/guru/springframework/converters/CategoryCommandToCategoryTest.java
@@ -1,46 +1,39 @@
 package guru.springframework.converters;
 
-import guru.springframework.commands.CategoryCommand;
-import guru.springframework.domain.Category;
-import org.junit.Before;
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import guru.springframework.commands.CategoryCommand;
+import guru.springframework.domain.Category;
 
 public class CategoryCommandToCategoryTest {
 
-    public static final String ID_VALUE = "1";
-    public static final String DESCRIPTION = "description";
-    CategoryCommandToCategory conveter;
+    private CategoryCommandToCategory converter = new CategoryCommandToCategory();
 
-    @Before
-    public void setUp() throws Exception {
-        conveter = new CategoryCommandToCategory();
+    @Test
+    public void testNullObject() {
+        assertThat(converter.convert(null)).isNull();
     }
 
     @Test
-    public void testNullObject() throws Exception {
-        assertNull(conveter.convert(null));
-    }
-
-    @Test
-    public void testEmptyObject() throws Exception {
-        assertNotNull(conveter.convert(new CategoryCommand()));
+    public void testEmptyObject() {
+        assertThat(converter.convert(new CategoryCommand())).isNotNull();
     }
 
     @Test
     public void convert() throws Exception {
         //given
-        CategoryCommand categoryCommand = new CategoryCommand();
-        categoryCommand.setId(ID_VALUE);
-        categoryCommand.setDescription(DESCRIPTION);
+        CategoryCommand command = new CategoryCommand();
+        command.setId("1");
+        command.setDescription("description");
 
         //when
-        Category category = conveter.convert(categoryCommand);
+        Category category = converter.convert(command);
 
         //then
-        assertEquals(ID_VALUE, category.getId());
-        assertEquals(DESCRIPTION, category.getDescription());
+        assertThat(category.getId()).isEqualTo(command.getId());
+        assertThat(category.getDescription()).isEqualTo(command.getDescription());
     }
 
 }
