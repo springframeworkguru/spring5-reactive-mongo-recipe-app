14bea1d63662a016faf3cbec209e412d9620a1df
springframeworkguru <john@springframework.guru>
2017-12-19 20:50:17 +0530

    Updating to Spring Boot M7 & Gradle wrapper version

--- src/main/java/guru/springframework/Spring5MongoRecipeAppApplication.java
+++ src/main/java/guru/springframework/Spring5MongoRecipeAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Spring5MongoRecipeAppApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(Spring5MongoRecipeAppApplication.class, args);
 	}
 }
