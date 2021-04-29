40c8ea1c5a552bc4cff36e348d84587383f08bf4
jmoussalli <jerome.moussalli@gmail.com>
2021-04-29 16:01:38 +0200

    checked

--- src/main/java/guru/springframework/commands/RecipeCommand.java
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -4,11 +4,11 @@ import guru.springframework.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.URL;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +22,7 @@ import java.util.List;
 public class RecipeCommand {
     private String id;
 
-    @NotBlank
+    @NotEmpty
     @Size(min = 3, max = 255)
     private String description;
 
@@ -42,7 +42,7 @@ public class RecipeCommand {
     @URL
     private String url;
 
-    @NotBlank
+    @NotEmpty
     private String directions;
 
     private List<IngredientCommand> ingredients = new ArrayList<>();
