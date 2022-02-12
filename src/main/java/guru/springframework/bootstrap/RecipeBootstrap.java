9b98086cbb1cd205ce38759e2263f664ddd7fb23
Trite8Q1 <60318513+Trite8Q1@users.noreply.github.com>
2022-02-12 10:28:22 +0100

    Update RecipeBootstrap
    The block() function has been added to the count log output. Because otherwise the desired result will not be output.

--- src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
+++ src/main/java/guru/springframework/bootstrap/RecipeBootstrap.java
@@ -49,7 +49,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
 
 
         log.error("#######");
-        log.error("Count: " + reactiveRepository.count());
+        log.error("Count: " + reactiveRepository.count().block());
     }
 
     private void loadCategories(){
