b6ce9b110c78fa27c8b29b82bf81dd30dff94be8
Eduard <edwardbs876@gmail.com>
2022-12-29 20:06:46 +0200

    Introducing reactive repositories

---
+++ src/main/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepository.java
@@ -0,0 +1,7 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+
+public interface UnitOfMeasureReactiveRepository extends ReactiveMongoRepository<UnitOfMeasure, String> {
+}
